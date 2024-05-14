cd22a40f4258c30e738a2149d3b39b6bc4063f79
AnderSecurity <anderson.ander.escola@hotmail.com>
2024-05-14 08:42:56 -0300

    First model-Controller-View - Department

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -33,6 +33,7 @@ namespace SalesWebMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
